338f15e5cb218680e6eef719e587787c64c21f37
Rory Eckel <16675082+roryeckel@users.noreply.github.com>
2021-07-14 15:58:03 +0100

    Generate ssh key and log

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ touch /root/.ssh/authorized_keys
 touch /root/.ssh/known_hosts
 chmod 0600 /root/.ssh/*
 
+ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ""
+cat /root/.ssh/id_rsa.pub
 echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
 echo "${KNOWN_HOSTS}" > /root/.ssh/known_hosts
 
